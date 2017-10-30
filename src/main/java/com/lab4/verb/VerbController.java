0eb02794545fbee859ceccb618b33624771446de
teresa-carmona <31965190+teresa-carmona@users.noreply.github.com>
2017-10-30 16:00:19 +0100

    add GetMapping annotation

--- src/main/java/com/lab4/verb/VerbController.java
+++ src/main/java/com/lab4/verb/VerbController.java
@@ -1,6 +1,7 @@
 package com.lab4.verb;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ public class VerbController {
     @Value("${words}")
     private String words;
 
+    @GetMapping("/")
     public @ResponseBody String getWord() {
         String[] wordArray = words.split(",");
         int i = (int)Math.round(Math.random() * (wordArray.length - 1));
